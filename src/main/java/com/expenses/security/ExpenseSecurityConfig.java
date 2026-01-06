0dd33107b4d019b9b963757da21b45e2a09d860f
vkarasas <vkarasas@gmail.com>
2026-01-06 21:21:21 +0200

    Delete unnecessary import

--- src/main/java/com/expenses/security/ExpenseSecurityConfig.java
+++ src/main/java/com/expenses/security/ExpenseSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,7 +26,6 @@ public class ExpenseSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) {
 
         http.authorizeHttpRequests(configurer ->
-
                         configurer
                                 .requestMatchers(
                                         "/**/*.css",
